589429ddb959faad40edf71fca24a3d04a873655
FAMILIA <dennisdiaz407@gmail.com>
2024-08-11 11:43:50 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import pyodbc as py
+import sqlalchemy as db
+from pandas.io import sql
+from scipy import stats
+import pymysql
+from datetime import datetime, date
+
+
+#Conexión a SQL Server
+con=db.create_engine('mssql+pyodbc://@localhost:1433/ANALISIS_DATOS?'+
+                     'driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes')
+
+
+#extracción de de los datasets
+fifa=pd.read_csv('fifa_ranking-2023-07-20.csv/fifa_ranking-2023-07-20.csv')
+juegos=pd.read_csv('athletes.csv/athletes.csv')
+covid=pd.read_csv('CovidDeaths.csv/CovidDeaths.csv')
+bbc=pd.read_csv('bbc_news.csv')
+pizza=pd.read_csv('pizza_sales.csv/pizza_sales.csv')
+concerts=pd.read_csv('concerts.csv')
+food=pd.read_csv('FastFoodNutritionMenuV3.csv')
+huff=pd.read_csv('HuffPost.csv')
+phish=pd.read_csv('phish.csv')
+spotify=pd.read_csv('Spotify_songs/Spotify_songs.csv')
+
+
+#vaerificación de la integridad de los datos y limpieza
+fifa.isnull().sum() #limpiado
+juegos.isnull().sum()
+juegos_rellenados=juegos.fillna("0")
+juegos_rellenados.isnull().sum() #limpiado
+covid.isnull().sum()
+covid_rellenado=covid.fillna("0")
+covid_rellenado.isnull().sum() #limpiado
+bbc.isnull().sum() #limpiado
+pizza.isnull().sum() #limpiado
+concerts.isnull().sum()
+concerts_rellenado=concerts.fillna("0")
+concerts_rellenado.isnull().sum() #limpiado
+food.isnull().sum()
+fooo_rellenado=food.fillna("0")
+fooo_rellenado.isnull().sum() #limpiado
+huff.isnull().sum()
+huff_rellenado=huff.fillna("0")
+huff_rellenado.isnull().sum() #limpiado
+phish.isnull().sum()
+phish_rellenado=phish.fillna("0")
+phish_rellenado.isnull().sum() #limpiado
+spotify.isnull().sum()
+spotify_rellenado=spotify.fillna("0")
+spotify_rellenado.isnull().sum() #limpiado
+
+
+#envío de los datasets como tablas a la base de datos definida en la conexión con SQL Server
+fifa.to_sql('fifa_rankings', con, index=False)
+juegos_rellenados.to_sql('juegos_olimpicos', con, index=False)
+covid_rellenado.to_sql('Covid-19', con, index=False)
+bbc.to_sql('BBC_News', con, index=False)
+pizza.to_sql('Dominios_Pizza', con, index=False)
+concerts_rellenado.to_sql('Orquesta de NY', con, index=False)
+fooo_rellenado.to_sql('Comida_rapida_restaurantes', con, index=False)
+huff_rellenado.to_sql('HuffPots', con, index=False)
+phish_rellenado.to_sql('Phish_Banda', con, index=False)
+spotify_rellenado.to_sql('Spotify_songs', con, index=False)
+
+
+#conexión y exportación de datos a MYSQL 
+fifa
+database_username='root'
+database_password='dataDennis'
+database_ip='localhost'
+database_name='analisis_datos'
+database_connection=db.create_engine('mysql+pymysql://{0}:{1}@{2}/{3}'.format(database_username, database_password, database_ip, database_name))
+conexion=database_connection.connect()
+metadata=db.MetaData()
+
+fifa.to_sql('fifa_rankings', con=conexion, index=False)
+juegos_rellenados.to_sql('juegos_olimpicos',  con=conexion, index=False)
+covid_rellenado.to_sql('Covid-19',  con=conexion, index=False)
+bbc.to_sql('BBC_News',  con=conexion, index=False)
+pizza.to_sql('Dominios_Pizza',  con=conexion, index=False)
+concerts_rellenado.to_sql('Orquesta de NY',  con=conexion, index=False)
+fooo_rellenado.to_sql('Comida_rapida_restaurantes',  con=conexion, index=False)
+huff_rellenado.to_sql('HuffPots',  con=conexion, index=False)
+phish_rellenado.to_sql('Phish_Banda', con=conexion, index=False)
+spotify_rellenado.to_sql('Spotify_songs', con=conexion, index=False)
